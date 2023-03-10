19dfe1e474007702090fbca19337cf6734f736ea
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2023-03-09 20:06:50 -0400

    Fix index style

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,4 @@
 class Appointment < ApplicationRecord
+    validates :name, :month, :day, presence: true
+    validates :day, numericality: { only_integer: true, greater_than: 0, less_than: 31 }
 end
